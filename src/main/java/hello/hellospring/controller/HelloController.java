8d2feba63d1b4098b0e316217e28500ab9c8fb18
chjh6107 <chjh6107@gmail.com>
2021-07-27 17:58:55 +0900

    add thymeleaf

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello") //웹앱에서 /hello로 치고 들어오면 아래 메서드 호출
+    //GetMapping은 GetPost와 같음
+    @GetMapping("hello") //웹앱에서 /hello로 들어오면 아래 메서드 호출
     public String hello(Model model){
-        model.addAttribute("data","테스트");
+        model.addAttribute("data","테스트"); //key,value
         return "hello";
     }
 }
