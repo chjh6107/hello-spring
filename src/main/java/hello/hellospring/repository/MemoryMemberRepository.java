0233e105d53cbb0f03ba6decf5399b2274ef9836
chjh6107 <chjh6107@gmail.com>
2021-07-29 23:30:17 +0900

    실습예제작성

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store=new HashMap<>();
+    private static long sequence=0;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(),member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> finById(Long id) {
+        //optional의 ofNullable 메서드 사용시 null이어도 감싸서 return 가능
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();//하나라도 찾으면 return
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
